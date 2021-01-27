ad33bbf543d33804783c55dc8aa0d594986dc993
Misha <xxxdaddy420@gmail.com>
2021-01-27 22:49:19 +0700

    fixed a bug with site scrolling when opening the menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $('.header_burger').click(function(event) {
        $('.header, .header_burger, .header_menu').toggleClass('active');
+       $('body').toggleClass('lock');
     });
 });
